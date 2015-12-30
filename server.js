43c99f745072ca87dfd0c2ec47d3e2d44fe41811
bojueWjt <jiataoTX@163.com>
2015-12-31 02:27:58 +0600

    node route

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-var http = require('http');
 
-http.createServer(function(request,response){
-	
-	response.writeHead(200,{'Content-Type':'text/plain'});
+var http = require("http");
+var url = require("url");
 
 
-	response.end('Hello World\n');
+function start(route){
 
-}).listen(8888);
+	function onRequest(request,response) {
 
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + "received.");
 
-console.log('Server running at http://127.0.0.1:8888/');
+		route(pathname);
+
+		response.writeHead(200,{"Content-Type": "text/plain"});
+		response.write("Hello World");
+
+		response.end();
+
+	}
+
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has started.");
+
+}
+
+exports.start = start;
+
